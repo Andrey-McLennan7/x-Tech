78db459d3768fe22bb21d6ed71762904c2ca9f1a
TheVirtualGamer752 <andreymclennan.work@gmail.com>
2025-01-15 20:00:17 +0000

    Added asteroid entity

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,4 +1,10 @@
 #include <x-Tech/x-Tech.h>
+
+#include "Controls.h"
+#include "Movement.h"
+#include "Shoot.h"
+#include "Data.h"
+
 #include <iostream>
 
 using namespace xTech;
@@ -31,8 +37,8 @@ int safe_main()
 	std::shared_ptr<Shader> gui_shader{ core->cache()->load<Shader>("Shader/gui") };
 
 	// Models
-	std::shared_ptr<Model> asteroid_model{ core->cache()->load<Model>("Model/FA59AMako/FA59AMako") };
-	std::shared_ptr<Model> ship_model{ core->cache()->load<Model>("Model/Meteorite/NHMW-MIN-J2669-Nakhla_low_res") };
+	std::shared_ptr<Model> asteroid_model{ core->cache()->load<Model>("Model/Meteorite/NHMW-MIN-J2669-Nakhla_low_res") };
+	std::shared_ptr<Model> ship_model{ core->cache()->load<Model>("Model/FA59AMako/FA59AMako") };
 
 	// Shapes
 	std::shared_ptr<Shape> bullet_shape{ core->cache()->load<Shape>("QUAD") };
@@ -40,6 +46,23 @@ int safe_main()
 
 	// Fonts
 	std::shared_ptr<Font> font{ core->cache()->load<Font>("Font/batmfa") };
+
+	// Adding entities
+	// Asteroid
+	std::shared_ptr<Entity> asteroid{ core->add_entity() };
+
+	asteroid->scale(0.1f);
+
+	// Add asteroid component
+	std::shared_ptr<ModelRenderer> asteroid_renderer{ asteroid->add_component<ModelRenderer>() };
+
+	asteroid_renderer->shader(model_shader);
+	asteroid_renderer->model(asteroid_model);
+
+	// Adjust the default camera
+	std::shared_ptr<Camera> camera{ core->camera() };
+
+	camera->position(vec3{ 0.0f, 0.0f, 50.0f });
 
 	// Prevent the window from being resized
 	core->window()->resizable(false);
