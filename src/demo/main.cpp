9fb07c2b123fbd24262db057bc79a049120cb5da
TheVirtualGamer752 <andreymclennan.work@gmail.com>
2025-01-15 19:40:55 +0000

    Loaded resources and added game classes

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -1,6 +1,7 @@
 #include <x-Tech/x-Tech.h>
-
 #include <iostream>
+
+using namespace xTech;
 
 int safe_main();
 
@@ -21,7 +22,24 @@ int main()
 int safe_main()
 {
 	// Create core
-	std::shared_ptr<xTech::Core> core{ xTech::Core::initialize() };
+	std::shared_ptr<Core> core{ Core::initialize() };
+
+	// Load resources
+	// Shaders
+	std::shared_ptr<Shader> model_shader{ core->cache()->load<Shader>("Shader/model") };
+	std::shared_ptr<Shader> basic_shader{ core->cache()->load<Shader>("Shader/basic") };
+	std::shared_ptr<Shader> gui_shader{ core->cache()->load<Shader>("Shader/gui") };
+
+	// Models
+	std::shared_ptr<Model> asteroid_model{ core->cache()->load<Model>("Model/FA59AMako/FA59AMako") };
+	std::shared_ptr<Model> ship_model{ core->cache()->load<Model>("Model/Meteorite/NHMW-MIN-J2669-Nakhla_low_res") };
+
+	// Shapes
+	std::shared_ptr<Shape> bullet_shape{ core->cache()->load<Shape>("QUAD") };
+	std::shared_ptr<Shape> star_shape{ core->cache()->load<Shape>("CUBE") };
+
+	// Fonts
+	std::shared_ptr<Font> font{ core->cache()->load<Font>("Font/batmfa") };
 
 	// Prevent the window from being resized
 	core->window()->resizable(false);
