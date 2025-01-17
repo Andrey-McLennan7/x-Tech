f9a1352351d7a196adaf1f3327bdcb8914e44dfc
Andrey McLennan (s5525305) <s5525305@bournemouth.ac.uk>
2025-01-17 13:57:49 +0000

    Added custom Star component

---
+++ src/demo/Star.cpp
@@ -0,0 +1,11 @@
+#include "Star.h"
+
+void CollisionResponce::on_initialize()
+{
+	Component::on_initialize();
+}
+
+void CollisionResponce::on_tick()
+{
+	Component::on_tick();
+}
---
+++ src/demo/Star.h
@@ -0,0 +1,21 @@
+#include <x-Tech/x-Tech.h>
+
+using namespace xTech;
+
+class Star : public Component
+{
+// Private data members
+private:
+
+
+
+// Public member functions
+public:
+
+	// First to execute when applied
+	virtual void on_initialize() override;
+
+	// Executed every iteration
+	virtual void on_tick() override;
+
+};
